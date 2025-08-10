b7aec5c6837850158002311c91e86b465a1b31af
Luis Almeida <122276206+lra2001@users.noreply.github.com>
2025-08-10 18:23:02 +0100

    Update admin.py
    - add tables Patient, Doctor and Appointment to admin

--- bestclinic_app/admin.py
+++ bestclinic_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Appointment, Patient, Doctor
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'lastname', 'date_of_birth', 'phone_number', 'email')
+    search_fields = ('firstname', 'lastname', 'email')
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('firstname', 'lastname', 'specialty', 'phone_number', 'email')
+    search_fields = ('firstname', 'lastname', 'specialty', 'email')
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'doctor', 'date', 'time')
+    search_fields = ('patient__firstname', 'doctor__firstname', 'date')
+    list_filter = ('date', 'doctor')
 
 # Register your models here.
